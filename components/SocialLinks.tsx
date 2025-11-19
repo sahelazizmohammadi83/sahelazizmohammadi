838cfb038ea7887fa28a2255e968eab2beb132f1
Sahel <sahelazizmohammadi83@gmail.com>
2025-11-19 21:03:19 +0330

    پاکسازی M ها و اصلاح Header و Layout

--- components/SocialLinks.tsx
+++ components/SocialLinks.tsx
@@ -6,11 +6,10 @@ export default function SocialLinks() {
   return (
     <div className="flex flex-col items-center mt-4 mb-6" style={{ gap: "16px" }}>
     
-      <Link href="https://t.me/sahelazizmohammadi" target="_blank"
-      
-      className="text-[#1a9eeb] hover:underline">
-       برای تمرین های عالی و رایگان بیا اینجا=تلگرام
-      </Link>
+      <Link href="https://t.me/yourusername" target="_blank" rel="noopener noreferrer" 
+ className="text-[#1a9eeb] hover:underline">
+  برای تمرین های عالی و رایگان با ف.یلتر شک.ن  بیا اینجا=تلگرام
+</Link>
       <Link href="https://www.instagram.com/sahelazizm" target="_blank" className="text-[#a53e9c] hover:underline">
        برای راهکارهای سریع و خفن بیااینجا=اینستگرام
       </Link>
